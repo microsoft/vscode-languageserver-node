1bfb9e1771eb98a60ada480d39c886e19d0a3a80
Dirk Baeumer <dirkb@microsoft.com>
2015-11-09 11:30:02 +0100

    Added copyright statements

--- client/src/utils/terminateProcess.sh
+++ client/src/utils/terminateProcess.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 
 terminateTree() {
     for cpid in $(pgrep -P $1); do
