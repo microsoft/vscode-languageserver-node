c4dcc6468f21947a5c63203a0606227beb633830
Remy Suen <remy.suen@gmail.com>
2019-04-16 11:56:47 -0400

    Fix a small typo
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

--- protocol/src/protocol.selectionRange.proposed.ts
+++ protocol/src/protocol.selectionRange.proposed.ts
@@ -62,7 +62,7 @@ export interface SelectionRange {
 	range: Range;
 
 	/**
-	 * The parent selection range containing this range. Therfore `parent.range` must contain `this.range`.
+	 * The parent selection range containing this range. Therefore `parent.range` must contain `this.range`.
 	 */
 	parent?: SelectionRange;
 
