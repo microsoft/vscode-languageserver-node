f6b2f0e523629b85055cc940ab1d6204d636d1f1
Dirk Baeumer <dirkb@microsoft.com>
2019-08-28 17:38:33 +0200

    Compiler and dependency cleanup

---
+++ client-tests/src/runTests.ts
@@ -0,0 +1,28 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+import * as path from 'path'
+
+import { runTests } from 'vscode-test'
+
+async function go() {
+	try {
+		const extensionDevelopmentPath = path.resolve(__dirname, '..')
+		const extensionTestsPath = __dirname;
+
+		/**
+		 * Basic usage
+		 */
+		await runTests({
+			extensionDevelopmentPath,
+			extensionTestsPath
+		});
+	} catch (err) {
+		console.error('Failed to run tests')
+		process.exitCode = 1;
+	}
+}
+go();
