cc12d1a1c7df935012cdef5d085cdba04a7c8ebe
Dirk Baeumer <dirkb@microsoft.com>
2019-09-10 11:28:56 +0200

    Fix incorrect class usage

--- types/src/main.ts
+++ types/src/main.ts
@@ -1954,7 +1954,7 @@ export namespace SymbolInformation {
  * have two ranges: one that encloses its definition and one that points to
  * its most interesting range, e.g. the range of an identifier.
  */
-export class DocumentSymbol {
+export interface DocumentSymbol {
 
 	/**
 	 * The name of this symbol. Will be displayed in the user interface and therefore must not be
@@ -2349,7 +2349,7 @@ export namespace FormattingOptions {
  * A document link is a range in a text document that links to an internal or external resource, like another
  * text document or a web site.
  */
-export class DocumentLink {
+export interface DocumentLink {
 
 	/**
 	 * The range this link applies to.
