44def0b8b723d38707e6c9ab2e033177416d8c0e
Dirk Baeumer <dirkb@microsoft.com>
2019-09-23 12:42:22 +0200

    Add correct since tag

--- types/src/main.ts
+++ types/src/main.ts
@@ -2347,16 +2347,22 @@ export interface FormattingOptions {
 
 	/**
 	 * Trim trailing whitespaces on a line.
+	 *
+	 * @since 3.15.0
 	 */
 	trimTrailingWhitespace?: boolean;
 
 	/**
 	 * Insert a newline character at the end of the file if one does not exist.
+	 *
+	 * @since 3.15.0
 	 */
 	insertFinalNewline?: boolean;
 
 	/**
 	 * Trim all newlines after the final newline at the end of the file.
+	 *
+	 * @since 3.15.0
 	 */
 	trimFinalNewlines?: boolean;
 
