304ebe98582475481f10f64cf87ffa139591e6c4
Dirk Baeumer <dirkb@microsoft.com>
2019-11-29 11:22:58 +0100

    Add more integration tests

--- client-tests/src/integration.test.ts
+++ client-tests/src/integration.test.ts
@@ -10,54 +10,55 @@ import * as lsclient from '../../client/lib/main';
 
 suite('Client integration', () => {
 
-	test('InitializeResult', (done) => {
+	let client!: lsclient.LanguageClient;
+
+	before(async (done) => {
 		let serverModule = path.join(__dirname, './servers/testInitializeResult.js');
 		let serverOptions: lsclient.ServerOptions = {
 			run: { module: serverModule, transport: lsclient.TransportKind.ipc },
 			debug: { module: serverModule, transport: lsclient.TransportKind.ipc, options: { execArgv: ['--nolazy', '--inspect=6014'] } }
 		};
-		let documentSelector: lsclient.DocumentSelector = ['css'];
+		let documentSelector: lsclient.DocumentSelector = ['plaintext'];
+
 		let clientOptions: lsclient.LanguageClientOptions = {
-			documentSelector, synchronize: {}, initializationOptions: {},
-			middleware: {
-				handleDiagnostics: (uri, diagnostics, next) => {
-					assert.equal(uri, 'uri:/test.ts');
-					assert.ok(Array.isArray(diagnostics));
-					assert.equal(diagnostics.length, 0);
-					next(uri, diagnostics);
-					disposable.dispose();
-					done();
+			documentSelector, synchronize: {}, initializationOptions: {}
+			// middleware: {
+			// 	handleDiagnostics: (uri, diagnostics, next) => {
+			// 		assert.equal(uri, 'uri:/test.ts');
+			// 		assert.ok(Array.isArray(diagnostics));
+			// 		assert.equal(diagnostics.length, 0);
+			// 		next(uri, diagnostics);
+			// 		disposable.dispose();
+			// 		done();
+			// 	}
+			// }
+		};
+
+		client = new lsclient.LanguageClient('css', 'Test Language Server', serverOptions, clientOptions);
+		client.start();
+		await client.onReady();
+		done();
+	});
+
+	after(async (done) => {
+		await client.stop();
+		done();
+	});
+
+	test('InitializeResult', () => {
+		let expected = {
+			capabilities: {
+				textDocumentSync: 1,
+				completionProvider: { resolveProvider: true, triggerCharacters: ['"', ':'] },
+				hoverProvider: true,
+				renameProvider: {
+					prepareProvider: true
 				}
+			},
+			customResults: {
+				'hello': 'world'
 			}
 		};
-		let client = new lsclient.LanguageClient('css', 'Test Language Server', serverOptions, clientOptions);
-		let disposable = client.start();
-
-		assert.equal(client.initializeResult, undefined);
-
-		client.onReady().then(_ => {
-			try {
-				let expected = {
-					capabilities: {
-						textDocumentSync: 1,
-						completionProvider: { resolveProvider: true, triggerCharacters: ['"', ':'] },
-						hoverProvider: true,
-						renameProvider: {
-							prepareProvider: true
-						}
-					},
-					customResults: {
-						'hello': 'world'
-					}
-				};
-				assert.deepEqual(client.initializeResult, expected);
-			} catch (e) {
-				disposable.dispose();
-				done(e);
-			}
-		}, e => {
-			disposable.dispose();
-			done(e);
-		});
-	}).timeout(10000);
+		assert.deepEqual(client.initializeResult, expected);
+	});
 });
