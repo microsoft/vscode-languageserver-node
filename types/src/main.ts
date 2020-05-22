02be110d6208162ebe3435748aec2192317cf5ac
Hirokazu Hata <h.hata.ai.t@gmail.com>
2020-05-22 12:03:08 +0900

    fix InsertTextFormat comment

--- types/src/main.ts
+++ types/src/main.ts
@@ -1453,7 +1453,7 @@ export namespace InsertTextFormat {
 	 * the end of the snippet. Placeholders with equal identifiers are linked,
 	 * that is typing in one will update others too.
 	 *
-	 * See also: https://github.com/Microsoft/vscode/blob/master/src/vs/editor/contrib/snippet/common/snippet.md
+	 * See also: https://github.com/microsoft/vscode/blob/master/src/vs/editor/contrib/snippet/snippet.md
 	 */
 	export const Snippet: 2 = 2;
 }
