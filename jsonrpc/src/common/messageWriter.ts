04a09a9ed5b181c35a93bcce47ea0189061f2a84
Andrew Arnott <Andrew.arnott@Microsoft.com>
2020-06-11 10:08:58 -0600

    Document MessageReader/MessageWriter interfaces (#636)

--- jsonrpc/src/common/messageWriter.ts
+++ jsonrpc/src/common/messageWriter.ts
@@ -13,10 +13,22 @@ import { ContentEncoder, ContentTypeEncoder } from './encoding';
 const ContentLength: string = 'Content-Length: ';
 const CRLF = '\r\n';
 
+/** Writes JSON-RPC messages to an underlying transport. */
 export interface MessageWriter {
+	/** Raised whenever an error occurs while writing a message. */
 	readonly onError: Event<[Error, Message | undefined, number | undefined]>;
+
+	/** An event raised when the underlying transport has closed and writing is no longer possible. */
 	readonly onClose: Event<void>;
+
+	/**
+	 * Sends a JSON-RPC message.
+	 * @param msg The JSON-RPC message to be sent.
+	 * @description Implementations should guarantee messages are transmitted in the same order that they are received by this method.
+	 */
 	write(msg: Message): Promise<void>;
+
+	/** Releases resources incurred from writing or raising events. Does NOT close the underlying transport, if any. */
 	dispose(): void;
 }
 
