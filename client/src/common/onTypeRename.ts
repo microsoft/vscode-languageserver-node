c88a96d3c1be3fda77fe8a29caff1e42ab7eb4bf
Dirk Baeumer <dirkb@microsoft.com>
2020-11-17 15:58:22 +0100

    Fix broken typings file

--- client/src/common/onTypeRename.ts
+++ client/src/common/onTypeRename.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-/// <reference path="./vscode-proposed.d.ts" />
+/// <reference path="../../typings/vscode-proposed.d.ts" />
 
 import * as code from 'vscode';
 import * as proto from 'vscode-languageserver-protocol';
