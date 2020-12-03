c0e82d5e836069129c621a7df7f115b488eab519
Danny Tuppeny <danny@tuppeny.com>
2020-12-03 17:43:22 +0000

    Fix typos

--- protocol/src/common/protocol.fileOperations.ts
+++ protocol/src/common/protocol.fileOperations.ts
@@ -38,12 +38,12 @@ export interface FileOperationOptions {
 }
 
 export interface FileOperationRegistrationOptions {
-	patterns: FileOperationPatten[];
+	patterns: FileOperationPattern[];
 }
 
-interface FileOperationPatten {
+interface FileOperationPattern {
 	/**
-	 * The glob patterns to match. Glob patterns can have the following syntax:
+	 * The glob pattern to match. Glob patterns can have the following syntax:
 	 * - `*` to match one or more characters in a path segment
 	 * - `?` to match on one character in a path segment
 	 * - `**` to match any number of path segments, including none
