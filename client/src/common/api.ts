bf274d873fb915be6bed64e5093cbe512aa8db5e
Dirk Baeumer <dirkb@microsoft.com>
2020-12-14 22:34:10 +0100

    Fixes #708: Export FileFormattingOptions

--- client/src/common/api.ts
+++ client/src/common/api.ts
@@ -4,7 +4,7 @@
  * ------------------------------------------------------------------------------------------ */
 
 export * from 'vscode-languageserver-protocol';
-export { Converter as Code2ProtocolConverter } from './codeConverter';
+export { Converter as Code2ProtocolConverter, FileFormattingOptions } from './codeConverter';
 export { Converter as Protocol2CodeConverter } from './protocolConverter';
 export * from './client';
 export * from './commonClient';
