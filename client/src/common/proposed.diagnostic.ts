f1ed3c3ced0e2e82643ad55b0900f742b6bd04fe
Dirk Baeumer <dirkb@microsoft.com>
2021-03-17 16:10:26 +0100

    Adopt server to new protocol for diagnostics

--- client/src/common/proposed.diagnostic.ts
+++ client/src/common/proposed.diagnostic.ts
@@ -14,8 +14,7 @@ import {
 } from 'vscode-languageserver-protocol';
 
 import {
-	TextDocumentFeature, BaseLanguageClient, Middleware, DidOpenTextDocumentFeatureShape, DidChangeTextDocumentFeatureShape, DidSaveTextDocumentFeatureShape,
-	DidCloseTextDocumentFeatureShape, LSPCancellationError
+	TextDocumentFeature, BaseLanguageClient, Middleware, LSPCancellationError, DiagnosticPullMode
 } from './client';
 
 function ensure<T, K extends keyof T>(target: T, key: K): T[K] {
@@ -81,7 +80,7 @@ class DiagnosticFeatureProviderImpl implements DiagnosticFeatureProvider {
 	public readonly provider: DiagnosticProvider;
 	public readonly disposable: Disposable;
 
-	constructor(client: BaseLanguageClient, id: string, options: Proposed.DiagnosticRegistrationOptions) {
+	constructor(client: BaseLanguageClient, options: Proposed.DiagnosticRegistrationOptions) {
 		const diagnosticPullOptions = client.clientOptions.diagnosticPullOptions ?? { onType: true, onSave: false };
 		const documentSelector = options.documentSelector!;
 		const disposables: Disposable[] = [];
@@ -200,7 +199,7 @@ class DiagnosticFeatureProviderImpl implements DiagnosticFeatureProvider {
 			const changeFeature = client.getFeature(DidChangeTextDocumentNotification.method);
 			disposables.push(changeFeature.onNotificationSent((event) => {
 				const textDocument = event.original.document;
-				if (manages(textDocument) && event.original.contentChanges.length > 0) {
+				if ((diagnosticPullOptions.filter === undefined || !diagnosticPullOptions.filter(textDocument, DiagnosticPullMode.onType)) && manages(textDocument) && event.original.contentChanges.length > 0) {
 					pullDiagnostics(textDocument);
 				}
 			}));
@@ -209,7 +208,7 @@ class DiagnosticFeatureProviderImpl implements DiagnosticFeatureProvider {
 			const saveFeature = client.getFeature(DidSaveTextDocumentNotification.method);
 			disposables.push(saveFeature.onNotificationSent((event) => {
 				const textDocument = event.original;
-				if (manages(textDocument)) {
+				if ((diagnosticPullOptions.filter === undefined || !diagnosticPullOptions.filter(textDocument, DiagnosticPullMode.onSave)) && manages(textDocument)) {
 					pullDiagnostics(event.original);
 				}
 			}));
@@ -238,19 +237,10 @@ class DiagnosticFeatureProviderImpl implements DiagnosticFeatureProvider {
 	}
 }
 
-export class DiagnosticFeature extends TextDocumentFeature<boolean | Proposed.DiagnosticOptions, Proposed.DiagnosticRegistrationOptions, DiagnosticProviders> {
-
-	private readonly openFeature: DidOpenTextDocumentFeatureShape;
-	private readonly changeFeature: DidChangeTextDocumentFeatureShape;
-	private readonly saveFeature: DidSaveTextDocumentFeatureShape;
-	private readonly closeFeature: DidCloseTextDocumentFeatureShape;
+export class DiagnosticFeature extends TextDocumentFeature<boolean | Proposed.DiagnosticOptions, Proposed.DiagnosticRegistrationOptions, DiagnosticFeatureProvider> {
 
 	constructor(client: BaseLanguageClient) {
 		super(client, Proposed.DocumentDiagnosticRequest.type);
-		this.openFeature = client.getFeature(DidOpenTextDocumentNotification.method);
-		this.changeFeature = client.getFeature(DidChangeTextDocumentNotification.method);
-		this.saveFeature = client.getFeature(DidSaveTextDocumentNotification.method);
-		this.closeFeature = client.getFeature(DidCloseTextDocumentNotification.method);
 	}
 
 	public fillClientCapabilities(capabilities: ClientCapabilities & Proposed.$DiagnosticClientCapabilities): void {
@@ -272,154 +262,8 @@ export class DiagnosticFeature extends TextDocumentFeature<boolean | Proposed.Di
 		this.register({ id: id, registerOptions: options });
 	}
 
-	protected registerLanguageProvider(options: Proposed.DiagnosticRegistrationOptions): [Disposable, DiagnosticProviders] {
-		const documentSelector = options.documentSelector!;
-		const mode = Proposed.DiagnosticPullModeFlags.is(options.mode) ? options.mode : (Proposed.DiagnosticPullModeFlags.onOpen | Proposed.DiagnosticPullModeFlags.onType);
-		const disposables: Disposable[] = [];
-		const collection = Languages.createDiagnosticCollection(options.identifier);
-		disposables.push(collection);
-		const availableEditors: Set<string> = new Set();
-		const managedDocuments: Map<string, TextDocument> = new Map();
-
-		const matches = (textDocument: TextDocument): boolean => {
-			return Languages.match(documentSelector, textDocument) > 0 && availableEditors.has(textDocument.uri.toString());
-		};
-
-		const manages = (textDocument: TextDocument): boolean => {
-			return managedDocuments.has(textDocument.uri.toString());
-		};
-
-		const onDidChangeDiagnosticsEmitter = new EventEmitter<void>();
-		const provider: DiagnosticProvider = {
-			onDidChangeDiagnostics: onDidChangeDiagnosticsEmitter.event,
-			provideDiagnostics: (textDocument, context, token) => {
-				const client = this._client;
-				const provideDiagnostics: ProvideDiagnosticSignature = (textDocument, context, token) => {
-					const params: Proposed.DiagnosticParams = {
-						textDocument: { uri: client.code2ProtocolConverter.asUri(textDocument.uri) },
-						context: context
-					};
-					return client.sendRequest(Proposed.DiagnosticRequest.type, params, token).then((result) => {
-						if (result === null || !result.items) {
-							return { items: [] };
-						}
-						return { items: client.protocol2CodeConverter.asDiagnostics(result.items) };
-					}, (error) => {
-						return client.handleFailedRequest(Proposed.DiagnosticRequest.type, token, error, { items: [] });
-					});
-				};
-				const middleware: Middleware & DiagnosticProviderMiddleware = client.clientOptions.middleware!;
-				return middleware.provideDiagnostics
-					? middleware.provideDiagnostics(textDocument, context, token, provideDiagnostics)
-					: provideDiagnostics(textDocument, context, token);
-			}
-		};
-
-		const requestStates: Map<string, RequestState> = new Map();
-		const pullDiagnostics = async (textDocument: TextDocument, trigger: Proposed.DiagnosticTriggerKind): Promise<void> => {
-			const key = textDocument.uri.toString();
-			const currentState = requestStates.get(key);
-			if (currentState !== undefined) {
-				if (currentState.state === RequestStateKind.active) {
-					currentState.tokenSource.cancel();
-				}
-				requestStates.set(key, { state: RequestStateKind.reschedule, textDocument, trigger: trigger });
-				// We have a state. Wait until the request returns.
-				return;
-			}
-			const tokenSource = new CancellationTokenSource();
-			requestStates.set(key, { state: RequestStateKind.active, textDocument, trigger, tokenSource });
-			let diagnostics: VDiagnosticResult | undefined;
-			let afterState: RequestState | undefined;
-			try {
-				diagnostics = await provider.provideDiagnostics(textDocument, { triggerKind: trigger }, tokenSource.token) ?? { items: [] };
-			} catch (error: unknown) {
-				if (error instanceof LSPCancellationError && Proposed.DiagnosticServerCancellationData.is(error.data) && error.data.retriggerRequest === false) {
-					afterState = { state: RequestStateKind.outDated, textDocument };
-				}
-				if (afterState === undefined && error instanceof CancellationError) {
-					afterState = { state: RequestStateKind.reschedule, textDocument, trigger };
-				} else {
-					throw error;
-				}
-			}
-			afterState = afterState ?? requestStates.get(key);
-			if (afterState === undefined) {
-				// This shouldn't happen. Log it
-				this._client.error(`Lost request state in diagnostic pull model. Clearing diagnostics for ${key}`);
-				collection.delete(textDocument.uri);
-				return;
-			}
-			requestStates.delete(key);
-			if (afterState.state === RequestStateKind.outDated || !manages(textDocument)) {
-				return;
-			}
-			// diagnostics is only undefined if the request has thrown.
-			if (diagnostics !== undefined && VDiagnosticResult.hasItems(diagnostics)) {
-				collection.set(textDocument.uri, diagnostics.items);
-			}
-			if (afterState.state === RequestStateKind.reschedule) {
-				pullDiagnostics(textDocument, afterState.trigger);
-			}
-		};
-
-		const openEditorsHandler = () => {
-			availableEditors.clear();
-			for (const info of Window.openEditors) {
-				availableEditors.add(info.resource.toString());
-			}
-		};
-		openEditorsHandler();
-		disposables.push(Window.onDidChangeOpenEditors(openEditorsHandler));
-
-		if (Proposed.DiagnosticPullModeFlags.isOpen(mode)) {
-			disposables.push(this.openFeature.onNotificationSent((event) => {
-				const textDocument = event.original;
-				if (matches(textDocument)) {
-					managedDocuments.set(textDocument.uri.toString(), textDocument);
-					pullDiagnostics(event.original, Proposed.DiagnosticTriggerKind.Opened);
-				}
-			}));
-			// Pull all diagnostics for documents that are already open
-			for (const textDocument of this.openFeature.openDocuments) {
-				if (matches(textDocument)) {
-					managedDocuments.set(textDocument.uri.toString(), textDocument);
-					pullDiagnostics(textDocument, Proposed.DiagnosticTriggerKind.Opened);
-				}
-			}
-		}
-		if (Proposed.DiagnosticPullModeFlags.isType(mode)) {
-			disposables.push(this.changeFeature.onNotificationSent((event) => {
-				const textDocument = event.original.document;
-				if (manages(textDocument) && event.original.contentChanges.length > 0) {
-					pullDiagnostics(textDocument, Proposed.DiagnosticTriggerKind.Typed);
-				}
-			}));
-		}
-		if (Proposed.DiagnosticPullModeFlags.isSave(mode)) {
-			disposables.push(this.saveFeature.onNotificationSent((event) => {
-				const textDocument = event.original;
-				if (manages(textDocument)) {
-					pullDiagnostics(event.original, Proposed.DiagnosticTriggerKind.Saved);
-				}
-			}));
-		}
-		disposables.push(this.closeFeature.onNotificationSent((event) => {
-			const textDocument = event.original;
-			const requestState = requestStates.get(textDocument.uri.toString());
-			if (requestState !== undefined) {
-				requestStates.set(textDocument.uri.toString(),{ state: RequestStateKind.outDated, textDocument });
-			}
-			if (manages(textDocument)) {
-				collection.delete(textDocument.uri);
-				managedDocuments.delete(textDocument.uri.toString());
-			}
-		}));
-		onDidChangeDiagnosticsEmitter.event(() => {
-			for (const document of managedDocuments.values()) {
-				pullDiagnostics(document, Proposed.DiagnosticTriggerKind.Invoked);
-			}
-		});
-		return [Disposable.from(...disposables), { onDidChangeDiagnosticsEmitter, provider }];
+	protected registerLanguageProvider(options: Proposed.DiagnosticRegistrationOptions): [Disposable, DiagnosticFeatureProvider] {
+		const provider = new DiagnosticFeatureProviderImpl(this._client, options);
+		return [provider.disposable, provider];
 	}
 }
