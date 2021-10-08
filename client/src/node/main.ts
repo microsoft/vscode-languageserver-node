5ee91c4ed2fa441f63c93effc77077e4e10968ff
Dirk Baeumer <dirkb@microsoft.com>
2021-10-08 09:33:45 +0200

    Publish client@8.0.0-next.3

--- client/src/node/main.ts
+++ client/src/node/main.ts
@@ -22,7 +22,7 @@ import { StreamMessageReader, StreamMessageWriter, IPCMessageReader, IPCMessageW
 export * from 'vscode-languageserver-protocol/node';
 export * from '../common/api';
 
-const REQUIRED_VSCODE_VERSION = '^1.58.0'; // do not change format, updated by `updateVSCode` script
+const REQUIRED_VSCODE_VERSION = '^1.61.0'; // do not change format, updated by `updateVSCode` script
 
 export enum TransportKind {
 	stdio,
