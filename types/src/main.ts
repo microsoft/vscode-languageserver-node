c40046d70896fde13ac80d7077cf2b7a6b589922
Dirk Baeumer <dirkb@microsoft.com>
2021-10-22 18:01:46 +0200

    Fixes #809: SignatureHelp interface does not agree with specification

--- types/src/main.ts
+++ types/src/main.ts
@@ -2392,16 +2392,28 @@ export interface SignatureHelp {
 	signatures: SignatureInformation[];
 
 	/**
-	 * The active signature. Set to `null` if no
-	 * signatures exist.
+	 * The active signature. If omitted or the value lies outside the
+	 * range of `signatures` the value defaults to zero or is ignored if
+	 * the `SignatureHelp` has no signatures.
+	 *
+	 * Whenever possible implementors should make an active decision about
+	 * the active signature and shouldn't rely on a default value.
+	 *
+	 * In future version of the protocol this property might become
+	 * mandatory to better express this.
 	 */
-	activeSignature: uinteger | null;
+	activeSignature?: uinteger;
 
 	/**
-	 * The active parameter of the active signature. Set to `null`
-	 * if the active signature has no parameters.
+	 * The active parameter of the active signature. If omitted or the value
+	 * lies outside the range of `signatures[activeSignature].parameters`
+	 * defaults to 0 if the active signature has parameters. If
+	 * the active signature has no parameters it is ignored.
+	 * In future version of the protocol this property might become
+	 * mandatory to better express the active parameter if the
+	 * active signature does have any.
 	 */
-	activeParameter: uinteger | null;
+	 activeParameter?: uinteger;
 }
 
 /**
