f3f227e2ec2c10e96199c9775f3e65ae3e0d9a66
Dirk Baeumer <dirkb@microsoft.com>
2021-11-15 11:48:06 +0100

    Remove unnecessary import

--- client/src/common/linkedEditingRange.ts
+++ client/src/common/linkedEditingRange.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-/// <reference path="../../typings/vscode-proposed.d.ts" />
 
 import * as code from 'vscode';
 import * as proto from 'vscode-languageserver-protocol';
