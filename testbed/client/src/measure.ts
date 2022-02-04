425ed0fcf88c6d851f7b1a5fa401f001fdd19f0f
Dirk Baeumer <dirkb@microsoft.com>
2022-02-04 15:01:08 +0100

    More measurements for JSON.parse

--- testbed/client/src/measure.ts
+++ testbed/client/src/measure.ts
@@ -17,52 +17,71 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('testbed.helloWorld', () => {
 
-		const counter = 5000;
+		const counter = 1000000;
 
 		const converter = p2c.createConverter(undefined, false, false);
-		const diagnostics: proto.Diagnostic[] = new Array(counter);
+		// const diagnostics: proto.Diagnostic[] = new Array(counter);
 
-		for (let i = 0; i < counter; i++) {
-			diagnostics[i] = proto.Diagnostic.create(
-				proto.Range.create(1,1,1,1),
-				'message',
-				proto.DiagnosticSeverity.Error,
-				'code',
-				'source',
-				[proto.DiagnosticRelatedInformation.create(
-					proto.Location.create('file:///abc/file.abc', proto.Range.create(2,2,2,2)),
-					'message'
-				)]
-			);
-		}
-
-		let start: number = 0;
-
-		// const arr = new Array(counter).fill(2, 0, counter);
-		// start = Date.now();
-		// const converted = arr.map(item => item * 2);
-		// console.log(`Time taken ${Date.now() - start}`);
-
-		// start = Date.now();
-		// const converted2 = new Array(arr.length);
-		// for (let i = 0; i < arr.length; i++) {
-		// 	converted2[i] = arr[i] * 2;
+		// for (let i = 0; i < counter; i++) {
+		// 	diagnostics[i] = proto.Diagnostic.create(
+		// 		proto.Range.create(1,1,1,1),
+		// 		'message',
+		// 		proto.DiagnosticSeverity.Error,
+		// 		'code',
+		// 		'source',
+		// 		[proto.DiagnosticRelatedInformation.create(
+		// 			proto.Location.create('file:///abc/file.abc', proto.Range.create(2,2,2,2)),
+		// 			'message'
+		// 		)]
+		// 	);
 		// }
-		// console.log(`Time taken ${Date.now() - start}`);
+
+		// let ranges: proto.Range[] = new Array(counter);
+		// for (let i = 0; i < ranges.length; i++) {
+		// 	ranges[i] = proto.Range.create(1, 1, 1, 1,);
+		// }
+		const message = JSON.stringify(new Array<proto.Range>(counter).fill(proto.Range.create(1, 1, 1, 1,), 0, counter));
+
+		console.time('reviver');
+		JSON.parse(message, (_key, value) => {
+			if (value.start !== undefined && value.end !== undefined) {
+				const start = (value as proto.Range).start;
+				const end = (value as proto.Range).end;
+				return new vscode.Range(start.line, start.character, end.line, end.character);
+			}
+			return value;
+		});
+		console.timeEnd('reviver');
+
+		console.time('without reviver');
+		const trad: vscode.Range[] = (JSON.parse(message) as proto.Range[]).map(range => converter.asRange(range));
+		console.timeEnd('without reviver');
 
 		// const message = JSON.stringify(diagnostics);
-		// start = Date.now();
+		// console.log(`Size of string message: ${message.length}`);
+		// console.time('Time taken to parse LSP diagnostics');
 		// const parsed = JSON.parse(message);
-		// const zero = `Time taken to parse LSP diagnostics ${Date.now() - start } ms`;
+		// console.timeEnd('Time taken to parse LSP diagnostics');
+		// let p = 0;
+		// console.time('Time taken to find id');
+		// for (let i = 0; i < message.length; i++) {
+		// 	if (message[i] === '{') {
+		// 		p++;
+		// 	}
+		// 	if (message[i] === '}') {
+		// 		p--;
+		// 	}
+		// }
+		// console.timeEnd('Time taken to find id');
 
-		start = Date.now();
-		const converted = converter.asDiagnostics(diagnostics);
-		const first = `Time taken to convert LSP into VS Code ${Date.now() - start } ms`;
+		// start = Date.now();
+		// const converted = converter.asDiagnostics(diagnostics);
+		// const first = `Time taken to convert LSP into VS Code ${Date.now() - start } ms`;
 
-		let asyncStart = Date.now();
-		void converter.asDiagnosticsAsync(diagnostics, new vscode.CancellationTokenSource().token).then((_converted) => {
-			console.log(`Time taken to convert LSP into VSCode async ${Date.now() - asyncStart }`);
-		});
+		// let asyncStart = Date.now();
+		// void converter.asDiagnosticsAsync(diagnostics, new vscode.CancellationTokenSource().token).then((_converted) => {
+		// 	console.log(`Time taken to convert LSP into VSCode async ${Date.now() - asyncStart }`);
+		// });
 
 		// const direct: vscode.Diagnostic[] = new Array(counter);
 		// start = Date.now();
@@ -90,8 +109,8 @@ export function activate(context: vscode.ExtensionContext) {
 		// }
 		// const third = `Time taken create VS Code diagnostics with literals ${Date.now() - start } ms`;
 
-		//console.log(zero);
-		console.log(first);
+		// console.log(zero);
+		// console.log(first);
 		// console.log(second);
 		// console.log(third);
 
