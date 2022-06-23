668411ec464bff7a397863e5af27c54941947a93
Michael Peyton Jones <me@michaelpj.com>
2022-06-23 17:45:51 +0100

    Fix showDocument

--- protocol/src/common/protocol.showDocument.ts
+++ protocol/src/common/protocol.showDocument.ts
@@ -77,7 +77,7 @@ export interface ShowDocumentResult {
 */
 export namespace ShowDocumentRequest {
 	export const method: 'window/showDocument' = 'window/showDocument';
-	export const messageDirection: MessageDirection = MessageDirection.clientToServer;
+	export const messageDirection: MessageDirection = MessageDirection.serverToClient;
 	export const type = new ProtocolRequestType<ShowDocumentParams, ShowDocumentResult, void, void, void>(method);
 	export type HandlerSignature = RequestHandler<ShowDocumentParams, ShowDocumentResult, void>;
 	export type MiddlewareSignature = (params: ShowDocumentParams, next: HandlerSignature) => HandlerResult<ShowDocumentResult, void>;
