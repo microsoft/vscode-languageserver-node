d7880d0175ab25dd2d5aa99cadc96ea4ee590433
Dirk Baeumer <dirkb@microsoft.com>
2022-12-15 09:25:01 +0100

    Fixes #992: Can initial contents of a document be set through willCreateFiles response WorkspaceEdit?

--- protocol/src/common/protocol.fileOperations.ts
+++ protocol/src/common/protocol.fileOperations.ts
@@ -281,6 +281,10 @@ export interface FileDelete {
  * The will create files request is sent from the client to the server before files are actually
  * created as long as the creation is triggered from within the client.
  *
+ * The request can return a `WorkspaceEdit` which will be applied to workspace before the
+ * files are created. Hence the `WorkspaceEdit` can not manipulate the content of the file
+ * to be created.
+ *
  * @since 3.16.0
  */
 export namespace WillCreateFilesRequest {
