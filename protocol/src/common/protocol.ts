769b690e81d9b26380219d461ee3a551df652483
Dirk Baeumer <dirkb@microsoft.com>
2023-08-09 15:58:12 +0200

    Fixes #1279: Links in the metaModel description sometimes uses markdown style

--- protocol/src/common/protocol.ts
+++ protocol/src/common/protocol.ts
@@ -2761,10 +2761,9 @@ export interface DefinitionRegistrationOptions extends TextDocumentRegistrationO
 
 /**
  * A request to resolve the definition location of a symbol at a given text
- * document position. The request's parameter is of type [TextDocumentPosition]
- * (#TextDocumentPosition) the response is of either type {@link Definition}
- * or a typed array of {@link DefinitionLink} or a Thenable that resolves
- * to such.
+ * document position. The request's parameter is of type {@link TextDocumentPosition}
+ * the response is of either type {@link Definition} or a typed array of
+ * {@link DefinitionLink} or a Thenable that resolves to such.
  */
 export namespace DefinitionRequest {
 	export const method: 'textDocument/definition' = 'textDocument/definition';
@@ -2847,9 +2846,9 @@ export interface DocumentHighlightRegistrationOptions extends TextDocumentRegist
 
 /**
  * Request to resolve a {@link DocumentHighlight} for a given
- * text document position. The request's parameter is of type [TextDocumentPosition]
- * (#TextDocumentPosition) the request response is of type [DocumentHighlight[]]
- * (#DocumentHighlight) or a Thenable that resolves to such.
+ * text document position. The request's parameter is of type {@link TextDocumentPosition}
+ * the request response is an array of type {@link DocumentHighlight}
+ * or a Thenable that resolves to such.
  */
 export namespace DocumentHighlightRequest {
 	export const method: 'textDocument/documentHighlight' = 'textDocument/documentHighlight';
