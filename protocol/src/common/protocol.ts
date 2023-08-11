5263a4e80f721c36f01670ddcfc1d1c2e9bf41d4
Dirk Baeumer <dirkb@microsoft.com>
2023-08-11 10:59:18 +0200

    Fixes #1293: metamodel documentation strings contains unmatched back-ticks

--- protocol/src/common/protocol.ts
+++ protocol/src/common/protocol.ts
@@ -149,21 +149,21 @@ export type TextDocumentFilter = {
 	language: string;
 	/** A Uri {@link Uri.scheme scheme}, like `file` or `untitled`. */
 	scheme?: string;
-	/** A glob pattern, like **​/*.{ts,js}. See TextDocumentFilter for examples`. */
+	/** A glob pattern, like **​/*.{ts,js}. See TextDocumentFilter for examples. */
 	pattern?: string;
 } | {
 	/** A language id, like `typescript`. */
 	language?: string;
 	/** A Uri {@link Uri.scheme scheme}, like `file` or `untitled`. */
 	scheme: string;
-	/** A glob pattern, like **​/*.{ts,js}. See TextDocumentFilter for examples`. */
+	/** A glob pattern, like **​/*.{ts,js}. See TextDocumentFilter for examples. */
 	pattern?: string;
 } | {
 	/** A language id, like `typescript`. */
 	language?: string;
 	/** A Uri {@link Uri.scheme scheme}, like `file` or `untitled`. */
 	scheme?: string;
-	/** A glob pattern, like **​/*.{ts,js}. See TextDocumentFilter for examples`. */
+	/** A glob pattern, like **​/*.{ts,js}. See TextDocumentFilter for examples. */
 	pattern: string;
 };
 
