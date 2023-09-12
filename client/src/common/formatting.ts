2501f7a0bf4d1084ece2044ae0799ff9da4f2b6d
Dirk Baeumer <dirkb@microsoft.com>
2023-09-12 13:59:08 -0700

    Move to vscode version 1.82

--- client/src/common/formatting.ts
+++ client/src/common/formatting.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-/// <reference path="../../typings/vscode.proposed.formatMultipleRanges.d.ts" />
 
 import {
 	languages as Languages, Disposable, TextDocument, ProviderResult, Range as VRange, Position as VPosition, TextEdit as VTextEdit, FormattingOptions as VFormattingOptions,
