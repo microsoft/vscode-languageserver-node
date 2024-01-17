f946cc79a2fa88263ced697d42614d895a6ebe5e
Maria Solano <mariasolano@microsoft.com>
2024-01-17 09:51:29 -0800

    Allow undefined snippet edit annotation ID

--- types/src/main.ts
+++ types/src/main.ts
@@ -1508,7 +1508,8 @@ export namespace SnippetTextEdit {
 		return Is.objectLiteral(candidate)
 			&& Range.is(candidate.range)
 			&& StringValue.isSnippet(candidate.snippet)
-			&& (ChangeAnnotation.is(candidate.annotationId) || ChangeAnnotationIdentifier.is(candidate.annotationId));
+			&& (candidate.annotationId === undefined ||
+				(ChangeAnnotation.is(candidate.annotationId) || ChangeAnnotationIdentifier.is(candidate.annotationId)));
 	}
 }
 
