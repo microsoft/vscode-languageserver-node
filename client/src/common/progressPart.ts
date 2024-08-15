89c6f5cb47e64964b685f42e5063a575b2c54635
Dirk Bäumer <dirkb@microsoft.com>
2024-08-15 12:48:11 +0200

    Trigger PR flow (#1543)

--- client/src/common/progressPart.ts
+++ client/src/common/progressPart.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-
 import {
 	window as Window, Progress, ProgressLocation, CancellationToken, Disposable
 } from 'vscode';
