b8694f7946a17bcb21dc6b25723d75b8547888d8
Dirk Bäumer <dirkb@microsoft.com>
2024-10-04 12:32:16 +0000

    Test PR

--- server/src/common/api.ts
+++ server/src/common/api.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-
 import { _, Features, _Connection, _LanguagesImpl } from './server';
 import { SemanticTokensBuilder } from './semanticTokens';
 import type { WorkDoneProgressReporter, WorkDoneProgressServerReporter, ResultProgressReporter } from './progress';
