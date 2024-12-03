18a3d80f6c12b62c8ecc3c0f46061fd63f2b2b16
Dirk Baeumer <dirkb@microsoft.com>
2024-12-03 16:53:18 +0100

    Move to latest compiler and NodeJS

--- server/src/common/server.ts
+++ server/src/common/server.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-/// <reference path="../../typings/thenable.d.ts" />
+/// <reference path="../../typings/thenable.d.ts" preserve: true/>
 
 import {
 	CancellationToken, ProtocolRequestType0, RequestHandler0, ProtocolRequestType, RequestHandler, GenericRequestHandler, StarRequestHandler, HandlerResult,
