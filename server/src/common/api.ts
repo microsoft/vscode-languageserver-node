b0a8ea15096b00d87a65f9ed3f36e5826de14d4c
Dirk Bäumer <dirkb@microsoft.com>
2026-06-15 17:02:35 +0200

    Make `TextDocumentContentFeature` a supported capability (#1796)
    Fixes #1795

--- server/src/common/api.ts
+++ server/src/common/api.ts
@@ -7,9 +7,6 @@ import { _, Features, _Connection, _LanguagesImpl } from './server';
 import { SemanticTokensBuilder } from './semanticTokens';
 import type { WorkDoneProgressReporter, WorkDoneProgressServerReporter, ResultProgressReporter } from './progress';
 
-import * as ic from './inlineCompletion.proposed';
-import * as tdc from './textDocumentContent';
-
 export * from 'vscode-languageserver-protocol';
 export { WorkDoneProgressReporter, WorkDoneProgressServerReporter, ResultProgressReporter };
 export { SemanticTokensBuilder };
@@ -20,11 +17,9 @@ export { NotebookDocuments };
 export * from './server';
 
 export namespace ProposedFeatures {
-	export const all: Features<_, _, _, _, _, tdc.TextDocumentContentFeatureShape, ic.InlineCompletionFeatureShape, _> = {
+	export const all: Features<_, _, _, _, _, _, _, _> = {
 		__brand: 'features',
-		workspace: tdc.TextDocumentContentFeature,
-		languages: ic.InlineCompletionFeature
 	};
 
-	export type Connection = _Connection<_, _, _, _, _, tdc.TextDocumentContentFeatureShape, ic.InlineCompletionFeatureShape, _>;
+	export type Connection = _Connection<_, _, _, _, _, _, _, _>;
 }
